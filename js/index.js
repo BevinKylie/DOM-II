a2e7ba6aaeb1c15cd432a73630a6ab197b16d5bf
Michael Trew <mtrew2015@gmail.com>
2019-12-05 23:32:21 -0500

    updates

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // Your code goes here
+const body = document.querySelector('body');
+console.log(body)
+body.addEventListener('keypress', (e) => {
+    e.target.style.backgroundColor ="lightgray"
+})
+
+const mapImg = document.querySelector('[src="img/adventure.jpg"]')
+mapImg.addEventListener('mouseenter', (e) => {
+    e.target.style.transform = ("rotate(30deg) transLateX(50px)")
+    // e.target.style.transform = ("translateX(50px)")
+})
+mapImg.addEventListener('mouseleave', (e) => {
+    e.target.style.transform = ("rotate(0deg) transLateX(0px)")
+    // e.target.style.transform = ("translateX(50px)")
+})
+
+const sound = document.getElementById('busSound');
+console.log(sound)
+const busPic = document.querySelector('[src="img/fun-bus.jpg"]')
+console.log(busPic)
+
+busPic.addEventListener('dblclick', (e) => {
+    sound.play()
+})
